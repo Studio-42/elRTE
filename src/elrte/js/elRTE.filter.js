d1412c769476c144f6c2ded90efc66861a7c5879
Troex Nevelin <troex@fury.scancode.ru>
2010-08-16 23:11:46 +0400

    progress on #154

--- src/elrte/js/elRTE.filter.js
+++ src/elrte/js/elRTE.filter.js
@@ -365,7 +365,7 @@
 					h = parseInt(t.css('height'))||'',
 					wm = t.attr('wmode'),
 					s = t.attr('style')
-					obj = '<embed type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" src="'+t.attr('rel')+'" width="'+w+'" height="'+h+'" style="'+s+'" play="true" loop="true" menu="true" wmode="'+wm+'"> </embed>';
+					obj = '<embed type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" src="'+t.attr('rel')+'" width="'+w+'" height="'+h+'" style="'+s+'" play="true" loop="true" menu="true" allowFullScreen="true" wmode="'+wm+'"> </embed>';
 					// obj = '<object style="'+(t.attr('style')||'')+'" width="'+parseInt(t.css('width'))+'" height="'+parseInt(t.css('height'))+'" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"><param name="quality" value="high" /><param name="movie" value="'+$(this).attr('rel')+'" /><embed pluginspage="http://www.macromedia.com/go/getflashplayer" quality="high" src="'+$(this).attr('rel')+'" type="application/x-shockwave-flash"></embed></object>';
 				// f.rte.log(obj)
 				t.replaceWith($(obj));
