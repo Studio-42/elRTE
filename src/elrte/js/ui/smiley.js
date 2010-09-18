e385a134efb0f1b94e9f10c62f56eca58fc68223
Troex Nevelin <troex@fury.scancode.ru>
2010-09-18 22:42:17 +0400

    smileys replaced with famfamfam.com ones

--- src/elrte/js/ui/smiley.js
+++ src/elrte/js/ui/smiley.js
@@ -10,60 +10,16 @@
 elRTE.prototype.ui.prototype.buttons.smiley = function(rte, name) {
 	this.constructor.prototype.constructor.call(this, rte, name);
 	var self = this;
-        this.img = null,
+        this.img = null;
 		this.url = this.rte.filter.url+'smileys/';
 		this.smileys = {
-			'smile' : 'smile.gif',
-			'wink' : 'wink.gif',
-			'give_heart' : 'give_heart.gif',
-			'give_rose' : 'give_rose.gif',
-			'heart' : 'heart.gif',
-			'acute' : 'acute.gif',
-			'air_kiss' : 'air_kiss.gif',
-			'angel' : 'angel.gif',
-			'blum' : 'blum.gif',
-			'blush' : 'blush.gif',
-			'clapping' : 'clapping.gif',
-			'crazy' : 'crazy.gif',
-			'dance' : 'dance.gif',
-			'dance4' : 'dance4.gif',
-			'dash1' : 'dash1.gif',
-			'diablo' : 'diablo.gif',
-			'dirol' : 'dirol.gif',
-			'drinks' : 'drinks.gif',
-			'first_move' : 'first_move.gif',
-			'focus' : 'focus.gif',
-			'girl_hide' : 'girl_hide.gif',
-			'girl_witch' : 'girl_witch.gif',
-			'good' : 'good.gif',
-			'help' : 'help.gif',
-			'kiss2' : 'kiss2.gif',
-			'kiss3' : 'kiss3.gif',
-			'lol' : 'lol.gif',
-			'mamba' : 'mamba.gif',
-			'mosking' : 'mosking.gif',
-			'music2' : 'music2.gif',
-			'ok' : 'ok.gif',
-			'paint3' : 'paint3.gif',
-			'pardon' : 'pardon.gif',
-			'preved' : 'preved.gif',
-			'sad' : 'sad.gif',
-			'scare' : 'scare.gif',
-			'scratch_one-s_head' : 'scratch_one-s_head.gif',
-			'secret' : 'secret.gif',
-			'shok' : 'shok.gif',
-			'spruce_up' : 'spruce_up.gif',
-			'tender' : 'tender.gif',
-			'unknw' : 'unknw.gif',
-			'victory' : 'victory.gif',
-			'wacko2' : 'wacko2.gif',
-			'whistle3' : 'whistle3.gif',
-			'yahoo' : 'yahoo.gif',
-			'yes3' : 'yes3.gif',
-			'bomb' : 'bomb.gif',
-			'bye' : 'bye.gif'
-			
+			'smile' : 'smile.png',
+			'grin' : 'grin.png',
+			'surprised' : 'surprised.png',
+			'tongue' : 'tongue.png',
+			'unhappy' : 'unhappy.png'
 		};
+		this.width = 150;
 
 
 	this.command = function() {
@@ -74,7 +30,7 @@ elRTE.prototype.ui.prototype.buttons.smiley = function(rte, name) {
 		opts = {
 			dialog : {
 				height  : 'auto',
-				width   : 430,
+				width   : this.width,
 				title   : this.rte.i18n('Smiley'),
 				buttons : {}
 			}
@@ -84,7 +40,7 @@ elRTE.prototype.ui.prototype.buttons.smiley = function(rte, name) {
 		$.each(this.smileys, function(name, img) {
 			d.append($('<img src="'+url+img+'" title="'+name+'" id="'+name+'" class="elSmiley"/>').click(function() { self.set(this.id, d);	}));
 		});
-		d.append('<hr/>Smiley by <a href="http://www.kolobok.us/" target="_blank">www.kolobok.us</a>').open();
+		d.open();
 	}
 
 	this.update = function() {
