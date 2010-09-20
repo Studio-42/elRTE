4a64d1bc756940070969a477e108c2e4d296e823
Dmitry Levashov <dio@std42.ru>
2010-09-21 00:54:56 +0400

    command hr repared

--- src/js/commands/hr.js
+++ src/js/commands/hr.js
@@ -8,19 +8,15 @@
 	elRTE.prototype.commands.hr = function() {
 		this.title = 'Horizontal rule';
 		
-		this.bind = function() {
-			var self = this;
-		
-			this.rte.bind('wysiwyg', function() {
-				self._update(self.STATE_ENABLE);
-			}).bind('source close', function(e) {
-				e.data.id == self.rte.active.id && self._update(self.STATE_DISABLE);
-			});
-		}
-		
 		this._exec = function() {
 			return this.sel.deleteContents().insertNode(this.dom.create('hr'));
 		}
+		
+		this.events = {
+			'wysiwyg'      : function() { this._setState(this.STATE_ENABLE); },
+			'source close' : function(e) { e.data.id == this.rte.active.id && this._setState(this.STATE_DISABLE); }
+		}
+		
 	}
 	
 })(jQuery);
--- src/js/elRTE.options.js
+++ src/js/elRTE.options.js
@@ -93,7 +93,7 @@
 			
 			semantic   : ['ins', 'del', 'abbr', 'cite'],
 			misc       : ['nbsp', 'stopfloat', 'hr', 'pagebreak', 'blockquote', 'div'],
-			test : ['unlink', 'stopfloat', 'nbsp'],
+			test : ['unlink', 'stopfloat', 'nbsp', 'hr'],
 			
 			direction  : ['dirltr', 'dirrtl'],
 			
