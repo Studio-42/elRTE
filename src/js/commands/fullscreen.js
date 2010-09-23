91b7816bee26647e3d76d406c8828144f8966430
Dmitry Levashov <dio@std42.ru>
2010-09-23 17:47:46 +0400

    elrte.view updated

--- src/js/commands/fullscreen.js
+++ src/js/commands/fullscreen.js
@@ -1,100 +1,98 @@
-(function($) {
+/**
+ * @class elRTE command fullscreen
+ *
+ * Toggle editor between normal/fullscreen view
+ * @author Dmitry (dio) Levashov, dio@std42.ru
+ **/
+elRTE.prototype.commands.fullscreen = function() {
+	var self   = this;
+	this.title = 'Full screen';
+	/* view object */
+	this.view = this.rte.view;
+	/* editor node */
+	this.editor = this.rte.view.editor;
+	/* workzone node */
+	this.wz = this.rte.view.workzone;
+	/* workzone height */
+	this.height = 0;
+	/* difference between editor and workzone heights */
+	this.delta = 0;
+	/* editor fullscreen css class */
+	this._class = 'elrte-fullscreen';
+	
 	/**
-	 * @class elRTE command fullscreen
+	 * Update editor height on window resize in fullscreen view
 	 *
-	 * Toggle editor between normal/fullscreen view
-	 * @author Dmitry (dio) Levashov, dio@std42.ru
 	 **/
-	elRTE.prototype.commands.fullscreen = function() {
-		var self   = this;
-		this.title = 'Full screen';
-		/* view object */
-		this.view = this.rte.view;
-		/* editor node */
-		this.editor = this.rte.view.editor;
-		/* workzone node */
-		this.wz = this.rte.view.workzone;
-		/* workzone height */
-		this.height = 0;
-		/* difference between editor and workzone heights */
-		this.delta = 0;
-		/* editor fullscreen css class */
-		this._class = 'elrte-fullscreen';
-		
-		/**
-		 * Update editor height on window resize in fullscreen view
-		 *
-		 **/
-		function resize() {
-			self.wz.height($(window).height()-self.delta);
-			self.view.updateHeight();
-		}
-
-		/**
-		 * Toggle between normal/fullscreen view
-		 *
-		 **/
-		this._exec = function() {
-			var w = $(window),
-				e = this.editor,
-				p = e.parents().filter(function(i, n) { return  !/^(html|body)$/i.test(n.nodeName) && $(n).css('position') == 'relative'; }),
-				wz = this.wz,
-				v = this.view,
-				c = this._class,
-				f = e.hasClass(c),
-				rte = this.rte,
-				s = this.rte.selection,
-				a = $.browser.mozilla && this.rte.isWysiwyg() ? this.rte.active : false,
-				b;
-				
-			function save() {
-				b = a ? s.bookmark() : false;
-			}
-			
-			function restore() {
-				if (a) {
-					a.editor.add(a.source).toggle();
-					a.source[0].focus();
-					a.editor.add(a.source).toggle();
-					s.toBookmark([b[0].id, b[1].id]);
-				}
-			}
-			
-			save();	
-			p.css('position', f ? 'relative' : 'static');
-				
-			if (f) {
-				e.removeClass(c);
-				wz.height(this.height);
-				w.unbind('resize', resize);
-			} else {
-				e.addClass(c).removeAttr('style');
-				wz.height(w.height() - this.delta).css('width', '100%');
-				w.bind('resize', resize);
-			}
-			
-			v.updateHeight();
-			v.resizable(f);
-			restore();
-			this.update();
-		}
-		
-		/**
-		 * Return command state
-		 *
-		 * @return Number
-		 **/
-		this._getState = function() {
-			return this.rte.view.editor.hasClass(this._class) ? this.STATE_ACTIVE : this.STATE_ENABLE;
-		}
-		
-		this.events = {
-			'load' : function() { 
-				this._setState(this.STATE_ENABLE); 
-				this.height  = this.wz.height();
-				this.delta   = this.editor.outerHeight()-this.height;
-			}
-		}
+	function resize() {
+		self.wz.height($(window).height()-self.delta);
+		self.view.updateHeight();
 	}
 
-})(jQuery);
+	/**
+	 * Toggle between normal/fullscreen view
+	 *
+	 **/
+	this._exec = function() {
+		var w = $(window),
+			e = this.editor,
+			p = e.parents().filter(function(i, n) { return  !/^(html|body)$/i.test(n.nodeName) && $(n).css('position') == 'relative'; }),
+			wz = this.wz,
+			v = this.view,
+			c = this._class,
+			f = e.hasClass(c),
+			rte = this.rte,
+			s = this.rte.selection,
+			a = $.browser.mozilla && this.rte.isWysiwyg() ? this.rte.active : false,
+			b;
+			
+		function save() {
+			b = a ? s.bookmark() : false;
+		}
+		
+		function restore() {
+			if (a) {
+				a.editor.add(a.source).toggle();
+				a.source[0].focus();
+				a.editor.add(a.source).toggle();
+				s.toBookmark([b[0].id, b[1].id]);
+			}
+		}
+		
+		save();	
+		p.css('position', f ? 'relative' : 'static');
+			
+		if (f) {
+			e.removeClass(c);
+			wz.height(this.height);
+			w.unbind('resize', resize);
+		} else {
+			e.addClass(c).removeAttr('style');
+			wz.height(w.height() - this.delta).css('width', '100%');
+			w.bind('resize', resize);
+		}
+		
+		v.updateHeight();
+		v.resizable(f);
+		restore();
+		this.update();
+	}
+	
+	/**
+	 * Return command state
+	 *
+	 * @return Number
+	 **/
+	this._getState = function() {
+		return this.rte.view.editor.hasClass(this._class) ? this.STATE_ACTIVE : this.STATE_ENABLE;
+	}
+	
+	this.events = {
+		'load' : function() { 
+			this._setState(this.STATE_ENABLE); 
+			this.height  = this.wz.height();
+			this.delta   = this.editor.outerHeight()-this.height;
+		}
+	}
+}
+
--- src/js/elRTE.view.js
+++ src/js/elRTE.view.js
@@ -1,223 +1,189 @@
-(function($) {
-	/**
-	 * Class. elRTE renderer
-	 * 
-	 */
-	elRTE.prototype.view = function(rte) {
-		var self       = this;
-		this.rte       = rte;
-		this.toolbar   = $('<div class="elrte-toolbar" />');
-		this.tabsbar   = $('<ul class="elrte-tabsbar" />');
-		this.workzone  = $('<div class="elrte-workzone" />');
-		this.statusbar = $('<div class="elrte-statusbar" />');
-		this.editor    = $('<div class="elrte '+(this.rte.options.cssClass||'')+'" id="'+this.rte.id+'" />')
-			.append(this.toolbar.hide())
-			.append($('<div class="elrte-tabsbar-wrapper"/>').append(this.tabsbar.hide()))
-			.append(this.workzone)
-			.append(this.statusbar.hide())
-			.insertBefore(rte.target);
-			
-		if (rte.options.height>0) {
-			this.workzone.height(this.rte.options.height);
-		}
-		if (!rte.options.showToolbar) {
-			// this.toolbar.hide();
-		}
-		if ($.fn.sortable) {
-			this.tabsbar.sortable({ delay : 10});
-		}
+
+/**
+ * @Class. elRTE renderer
+ * @TODO move class namesito variables
+ */
+elRTE.prototype.view = function(rte) {
+	var self       = this;
+	this.rte       = rte;
+	this.toolbar   = $('<div class="elrte-toolbar" />');
+	this.tabsbar   = $('<ul class="elrte-tabsbar" />');
+	this.workzone  = $('<div class="elrte-workzone" />');
+	this.statusbar = $('<div class="elrte-statusbar" />');
+	this.editor    = $('<div class="elrte '+(this.rte.options.cssClass||'')+'" id="'+this.rte.id+'" />')
+		.append(this.toolbar.hide())
+		.append($('<div class="elrte-tabsbar-wrapper"/>').append(this.tabsbar.hide()))
+		.append(this.workzone)
+		.append(this.statusbar.hide())
+		.insertBefore(rte.target);
 		
-		/**
-		 * Add new document into editor
-		 *
-		 * @param  Object  document
-		 * @return void
-		 **/
-		this.add = function(d) {
-			var self = this,
-				h    = this.workzone.height(),
-				tab  = $('<li class="elrte-doc-tab elrte-ib" rel="#'+d.id+'" title="'+d.title+'"><div class="elrte-doc-tab-title">'+d.title+'</div></li>')
-					.appendTo(this.tabsbar)
-					.mousedown(function(e) {
-						e.preventDefault();
-						self.rte.focus($(this).attr('rel').substr(1));
-					});
-				
-			$('<div id="'+d.id+'" class="elrte-document"/>')
-				.append(d.editor.height(h))
-				.append(d.source.height(h).hide())
-				.hide()
-				.appendTo(this.workzone);
-			
-			if (this.rte.options.allowCloseDocs) {
-				tab.append($('<span class="elrte-doc-tab-close" title="'+this.rte.i18n('Close')+'"/>').mousedown(function(e) {
-					var p = $(this).parent();
-					e.preventDefault();
-					e.stopPropagation();
-					if (confirm(self.rte.i18n('Close')+' "'+p.text()+'"?')) {
-						self.rte.close(p.attr('rel').substr(1));
-					}
-				}));
-			}
-			this.updateTabsbar();
-		}
-
-		/**
-		 * Set document with required id visible and hide others
-		 *
-		 * @param String  document id
-		 * @return void
-		 */
-		this.showDoc = function(id) {
-			this.workzone.children('.elrte-document').hide().filter('#'+id).show();
-			this.tabsbar.children().removeClass('elrte-tab-active').filter('[rel="#'+id+'"]').addClass('elrte-tab-active');
-		}
-
-		/**
-		 * Remove document by id
-		 *
-		 * @param String  document id
-		 * @return void
-		 */
-		this.remove = function(id) {
-			this.workzone.find('#'+id).add(this.tabsbar.find('[rel="#'+id+'"]')).remove();
-			this.updateTabsbar();
-		}
-		
-		/**
-		 * Update tabsbar visibility
-		 *
-		 * @return void
-		 **/
-		this.updateTabsbar = function() {
-			self.tabsbar.toggle(self.workzone.children('.elrte-document').length > 1 || self.rte.options.alwaysShowTabs);
-		}
-		
-		/**
-		 * Returns next documents id after active one
-		 *
-		 * @return String
-		 */
-		this.getNext = function() {
-			var n = this.tabsbar.children('.elrte-tab-active').next();
-			return n.length ? n.attr('rel').substr(1) : false;
-		}
-
-		/**
-		 * Returns previous documents id after active one
-		 *
-		 * @return String
-		 */
-		this.getPrev = function() {
-			var n = this.tabsbar.children('.elrte-tab-active').prev();
-			return n.length ? n.attr('rel').substr(1) : false;
-		}
-
-		/**
-		 * Returns first document in editor
-		 *
-		 * @return String
-		 */
-		this.getFirst = function() {
-			var n = this.tabsbar.children('.elrte-doc-tab').eq(0);
-			return n.length ? n.attr('rel').substr(1) : false;
-		}
-		
-		/**
-		 * Returns last document in editor
-		 *
-		 * @return String
-		 */
-		this.getLast = function() {
-			var n = this.tabsbar.children('.elrte-doc-tab:last-child');
-			return n.length ? n.attr('rel').substr(1) : false;
-		}
-
-		this.buildUI = function(tb, commands) {
-			var self = this,
-				pl = tb.length,
-				pc = 'elrte-toolbar-panel',
-				cn, cl, cmd, bn, button, panel;
-			
-			while (pl--) {
-				panel = $('<ul class="'+pc+' '+pc+'-'+(pn = tb[pl])+'"/>');
-				cn = this.rte.options.panels[pn]||[];
-				cl = cn.length;
-				while (cl--) {
-					cmd = commands[cn[cl]]
-					if (cmd) {
-						button = this.rte.ui[cmd.button]||this.rte.ui.button;
-						panel.prepend(button(cmd))
-					}
-				}
-				if (panel.children().length) {
-					this.toolbar.prepend(panel)
-				}
-			}
-			
-			if (this.toolbar.children().length) {
-				this.toolbar.show();
-				this.toolbar.children().children().hover(function(e) {
-					var t = $(this);
-					t.toggleClass('elrte-ui-hover')
-					// self.rte.log(this)
-				})
-			}
-			
-		}
-
-		/**
-		 * Add button on required toolbar's panel 
-		 *
-		 * @param  jQuery  button
-		 * @param  String  panel name
-		 * @return void
-		 */
-		this.addUI = function(ui, pn) {
-			var cmd = this.rte.command,
-				c = 'elrte-toolbar-panel',
-				p = this.toolbar.show().children('.'+c+'-'+pn);
-				
-			if (!p.length) {
-				p = $('<ul class="'+c+' '+c+'-'+pn+(!this.toolbar.children('.'+c).length ? ' '+c+'-first' : '')+'" />').appendTo(this.toolbar);
-			}
-			ui.hover(function() {
-				var t = $(this);
-				
-				if (!t.hasClass(cmd.uiDisableClass)) {
-					t.toggleClass(cmd.uiHoverClass);
-				}
-			})
-			p.append(ui);
-		}
-
-		/**
-		 * Sync iframes/textareas height with workzone height 
-		 *
-		 * @return void
-		 */
-		this.updateHeight = function() {
-			self.workzone.find('.elrte-editor,.elrte-source').height(self.workzone.height());
-		}
-		
-		/**
-		 * Turn editor resizable on/off if allowed
-		 *
-		 * @param  Boolean 
-		 * @return void
-		 **/
-		this.resizable = function(r) {
-			if (this.rte.options.resizable && $.fn.resizable) {
-				if (r) {
-					this.editor.resizable({handles : 'se', alsoResize : this.workzone, minWidth :300, minHeight : 200 }).bind('resize', this.updateHeight);
-				} else {
-					this.editor.resizable('destroy').unbind('resize', this.updateHeight);
-				}
-			}
-		}
-		
-		this.resizable(true);
-
+	if (rte.options.height>0) {
+		this.workzone.height(this.rte.options.height);
+	}
+	if ($.fn.sortable) {
+		this.tabsbar.sortable({ delay : 10});
 	}
 	
-})(jQuery);
+	/**
+	 * Add new document into editor
+	 *
+	 * @param  Object  document
+	 * @return void
+	 **/
+	this.add = function(d) {
+		var self = this,
+			h    = this.workzone.height(),
+			tab  = $('<li class="elrte-doc-tab elrte-ib" rel="#'+d.id+'" title="'+d.title+'"><div class="elrte-doc-tab-title">'+d.title+'</div></li>')
+				.appendTo(this.tabsbar)
+				.mousedown(function(e) {
+					e.preventDefault();
+					self.rte.focus($(this).attr('rel').substr(1));
+				});
+			
+		$('<div id="'+d.id+'" class="elrte-document"/>')
+			.append(d.editor.height(h))
+			.append(d.source.height(h).hide())
+			.hide()
+			.appendTo(this.workzone);
+		
+		if (this.rte.options.allowCloseDocs) {
+			tab.append($('<span class="elrte-doc-tab-close" title="'+this.rte.i18n('Close')+'"/>').mousedown(function(e) {
+				var p = $(this).parent();
+				e.preventDefault();
+				e.stopPropagation();
+				if (confirm(self.rte.i18n('Close')+' "'+p.text()+'"?')) {
+					self.rte.close(p.attr('rel').substr(1));
+				}
+			}));
+		}
+		this.updateTabsbar();
+	}
+
+	/**
+	 * Set document with required id visible and hide others
+	 *
+	 * @param String  document id
+	 * @return void
+	 */
+	this.showDoc = function(id) {
+		this.workzone.children('.elrte-document').hide().filter('#'+id).show();
+		this.tabsbar.children().removeClass('elrte-tab-active').filter('[rel="#'+id+'"]').addClass('elrte-tab-active');
+	}
+
+	/**
+	 * Remove document by id
+	 *
+	 * @param String  document id
+	 * @return void
+	 */
+	this.remove = function(id) {
+		this.workzone.find('#'+id).add(this.tabsbar.find('[rel="#'+id+'"]')).remove();
+		this.updateTabsbar();
+	}
+	
+	/**
+	 * Update tabsbar visibility
+	 *
+	 * @return void
+	 **/
+	this.updateTabsbar = function() {
+		self.tabsbar.toggle(self.workzone.children('.elrte-document').length > 1 || self.rte.options.alwaysShowTabs);
+	}
+	
+	/**
+	 * Returns next documents id after active one
+	 *
+	 * @return String
+	 */
+	this.getNext = function() {
+		var n = this.tabsbar.children('.elrte-tab-active').next();
+		return n.length ? n.attr('rel').substr(1) : false;
+	}
+
+	/**
+	 * Returns previous documents id after active one
+	 *
+	 * @return String
+	 */
+	this.getPrev = function() {
+		var n = this.tabsbar.children('.elrte-tab-active').prev();
+		return n.length ? n.attr('rel').substr(1) : false;
+	}
+
+	/**
+	 * Returns first document in editor
+	 *
+	 * @return String
+	 */
+	this.getFirst = function() {
+		var n = this.tabsbar.children('.elrte-doc-tab').eq(0);
+		return n.length ? n.attr('rel').substr(1) : false;
+	}
+	
+	/**
+	 * Returns last document in editor
+	 *
+	 * @return String
+	 */
+	this.getLast = function() {
+		var n = this.tabsbar.children('.elrte-doc-tab:last-child');
+		return n.length ? n.attr('rel').substr(1) : false;
+	}
+
+	this.buildUI = function(tb, commands) {
+		var pl = tb.length,
+			pc = 'elrte-toolbar-panel',
+			pn, pnl, cn, cl, cmd, btn;
+		
+		while (pl--) {
+			pn  = tb[pl];
+			pnl = $('<ul class="'+pc+' '+pc+'-'+pn+'"/>');
+			cn  = this.rte.options.panels[pn]||[];
+			cl  = cn.length;
+			while (cl--) {
+				
+				if ((cmd = commands[cn[cl]])) {
+					btn = this.rte.ui[cmd.button]||this.rte.ui.button;
+					pnl.prepend(btn(cmd));
+				}
+			}
+			pnl.children().length && this.toolbar.prepend(pnl);
+		}
+
+		if (this.toolbar.children().length) {
+			this.toolbar.show().children().children().hover(function(e) {
+				$(this).toggleClass('elrte-ui-hover');
+			});
+		}
+		
+	}
+
+	/**
+	 * Sync iframes/textareas height with workzone height 
+	 *
+	 * @return void
+	 */
+	this.updateHeight = function() {
+		self.workzone.find('.elrte-editor,.elrte-source').height(self.workzone.height());
+	}
+	
+	/**
+	 * Turn editor resizable on/off if allowed
+	 *
+	 * @param  Boolean 
+	 * @return void
+	 **/
+	this.resizable = function(r) {
+		if (this.rte.options.resizable && $.fn.resizable) {
+			if (r) {
+				this.editor.resizable({handles : 'se', alsoResize : this.workzone, minWidth :300, minHeight : 200 }).bind('resize', this.updateHeight);
+			} else {
+				this.editor.resizable('destroy').unbind('resize', this.updateHeight);
+			}
+		}
+	}
+	
+	this.resizable(true);
+
+}
+
