8b6ead9544bd2b9883d3f934709414b65ea77dd3
Troex Nevelin <troex@fury.scancode.ru>
2010-12-10 03:24:28 +0300

    iframe support added

--- src/elrte/js/elRTE.filter.js
+++ src/elrte/js/elRTE.filter.js
@@ -37,7 +37,9 @@
 		this.embRegExp = /<(embed)((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*>/gi;
 		// param tag regexp
 		this.paramRegExp = /<(param)((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*>/gi;
-		this.vimeoRegExp = /<(iframe)\s+([^>]*src\s*=\s*"http:\/\/[^"]+vimeo\.com\/\w+[^>]*)>([\s\S]*?)<\/iframe>/gi;
+		// iframe tag regexp
+		this.iframeRegExp = /<iframe([^>]*)>([\s\S]*?)<\/iframe>/gi;
+
 		// yandex maps regexp
 		this.yMapsRegExp = /<div\s+([^>]*id\s*=\s*('|")?YMapsID[^>]*)>/gi;
 		// google maps regexp
@@ -625,6 +627,16 @@
 					a.height == '1' && delete a.height;
 					return i ? img({ embed : a }, i.type) : t;
 				})
+				.replace(this.iframeRegExp, function(t, a) {
+					var a = self.parseAttrs(a);
+					self.rte.log(a);
+					var w = a.style.width || (parseInt(a.width) > 1 ? parseInt(a.width)+'px' : '100px');
+					var h = a.style.height || (parseInt(a.height) > 1 ? parseInt(a.height)+'px' : '100px');
+					var id = 'iframe'+Math.random().toString().substring(2);
+					self.scripts[id] = t;
+					var img = '<img id="'+id+'" src="'+self.url+'pixel.gif" class="elrte-protected elrte-iframe" style="width:'+w+'; height:'+h+'">';
+					return img;
+				})
 				.replace(this.vimeoRegExp, function(t, n, a) {
 					a = self.parseAttrs(a);
 					delete a.frameborder;
@@ -736,7 +748,9 @@
 				.replace(this.serviceClassRegExp, function(t, n, a, e) {
 					var a = self.parseAttrs(a), j, o = '';
 					// alert(t)
-					if (a['class']['elrte-media']) {
+					if (a['class']['elrte-iframe']) {
+						return self.scripts[a.id] || '';
+					} else if (a['class']['elrte-media']) {
 						// alert(a.rel)
 						// return ''
 						// j = a.rel ? JSON.parse(self.rte.utils.decode(a.rel)) : {};
