72baa1fde2cce4e5cc924961e3284a25e16d7c5f
Dmitry Levashov <dio@std42.ru>
2010-12-16 02:17:34 +0300

    new jquery.elrte plugin

---
+++ src/js/jquery.elrte.js
@@ -0,0 +1,35 @@
+/**
+ * Extend jQuery expressions.
+ * Find elements in set which has elRTE editor instance
+ *
+ * @example
+ * Select only nodes with elRTE instances
+ *  $("selector:elrte")
+ */
+$.expr[':'].elrte = function(e) {
+	var inst = $(e).data('elrte-editor');
+	return !!(inst && inst.id);
+}
+
+$.fn.elrte = function(o) {
+	
+	this.each(function() {
+		var $this = $(this);
+		
+		if (!$this.data('elrte') && !$this.data('_elrte_lock')) {
+			$this.data('_elrte_lock', 1).data('elrte', new elRTE(o, this)).removeData('_elrte_lock');
+		}
+	});
+	
+	if (typeof(o) == 'string') {
+		var inst = $(this).getElrte();
+		
+		return inst ? inst.exec.apply(inst, arguments) : void(0);
+	}
+	
+	return this;
+}
+
+$.fn.getElrte = function() {
+	return this.eq(0).data('elrte');
+}
