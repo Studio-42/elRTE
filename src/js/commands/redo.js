c2df0b94ab968b59e1fa1f4db56bd84bbd09bb87
Dmitry Levashov <dio@std42.ru>
2010-12-25 14:53:14 +0300

    missed: plugins updated

--- src/js/commands/redo.js
+++ src/js/commands/redo.js
@@ -1,27 +1,24 @@
-(function($) {
+/**
+ * @class redo - redo button
+ *
+ * @param  elRTE
+ * @author Dmitry (dio) Levashov, dio@std42.ru
+ **/
+elRTE.prototype.commands.redo = function() {
+	this.title = 'Redo previous action';
 	
-	/**
-	 * @class redo - redo button
-	 *
-	 * @param  elRTE
-	 * @author Dmitry (dio) Levashov, dio@std42.ru
-	 **/
-	elRTE.prototype.commands.redo = function() {
-		this.title = 'Redo previous action';
-		
-		this._exec = function() {
-			return this.rte.history.redo();
-		}
-		
-		this._state = function() {
-			return this.rte.history.canRedo() ? elRTE.CMD_STATE_ENABLED : elRTE.CMD_STATE_DISABLED;
-		}
-		
-		this.events = {
-			'wysiwyg historyChange' : function(e) { this.update(); },
-			'close source' : function(e) { e.data.id == this.rte.active.id && this.update(elRTE.CMD_STATE_DISABLED); }
-		}
+	this._exec = function() {
+		return this.rte.history.redo();
 	}
-
 	
-})(jQuery);
+	this._state = function() {
+		return this.rte.history.canRedo() ? elRTE.CMD_STATE_ENABLED : elRTE.CMD_STATE_DISABLED;
+	}
+	
+	this.events = {
+		'wysiwyg historyChange' : function(e) { this.update(); },
+		'close source' : function(e) { e.data.id == this.rte.active.id && this.update(elRTE.CMD_STATE_DISABLED); }
+	}
+}
+
+
--- src/js/commands/undo.js
+++ src/js/commands/undo.js
@@ -1,29 +1,24 @@
-(function($) {
+/**
+ * @class undo - undo button
+ *
+ * @param  elRTE
+ * @author Dmitry (dio) Levashov, dio@std42.ru
+ **/
+elRTE.prototype.commands.undo = function() {
+	this.title = 'Undo last action';
 	
-	/**
-	 * @class undo - undo button
-	 *
-	 * @param  elRTE
-	 * @author Dmitry (dio) Levashov, dio@std42.ru
-	 **/
-	elRTE.prototype.commands.undo = function() {
-		this.title = 'Undo last action';
-		
-		
-		this._exec = function() {
-			return this.rte.history.undo();
-		}
-		
-		this._state = function() {
-			return this.rte.history.canUndo() ? elRTE.CMD_STATE_ENABLED : elRTE.CMD_STATE_DISABLED;
-		}
-		
-		this.events = {
-			'wysiwyg historyChange' : function(e) { this.update(); },
-			'close source' : function(e) { e.data.id == this.rte.active.id && this.update(elRTE.CMD_STATE_DISABLED); }
-		}
-		
+	
+	this._exec = function() {
+		return this.rte.history.undo();
 	}
-
 	
-})(jQuery);
+	this._state = function() {
+		return this.rte.history.canUndo() ? elRTE.CMD_STATE_ENABLED : elRTE.CMD_STATE_DISABLED;
+	}
+	
+	this.events = {
+		'wysiwyg historyChange' : function(e) { this.update(); },
+		'close source' : function(e) { e.data.id == this.rte.active.id && this.update(elRTE.CMD_STATE_DISABLED); }
+	}
+	
+}
