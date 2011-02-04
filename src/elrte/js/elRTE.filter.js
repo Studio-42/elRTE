Studio-42/elRTE

commit f2bdb7b6dd4738d7e9bc7717d7764a00d75c902e

enhanced xhtmlTags so that it can handle even strange attribute contents * this is quite usefull to edit template files (like Freemarker)
INPUT
<img src="<@specialTag>some/path</@specialTag>">

OUTPUT: elRTE (before this change)
<img src="<@specialTag />some/path</@specialTag>">

OUTPUT: expected (after this change)
<img src="<@specialTag>some/path</@specialTag>" />

src/elrte/js/elRTE.filter.js
@@ -840,7 +840,15 @@
 		 * return String
 		 **/
 		xhtmlTags : function(html) {
-			return this.xhtml ? html.replace(/<(img|hr|br|embed|param|link)([^>]*\/*)>/gi, "<$1$2 />") : html;
+			if (!this.xhtml) return html;
+
+			return html.replace(this.tagRegExp, function(fullMatch, closingSlash, tagName, tagAttributes) {
+				if (/^(img|hr|br|embed|param|link)$/i.test(name)) {
+					return '<' + tagName + tagAttributes + ' />';
+				} else {
+					return fullMatch;
+				}
+			});
 		}
 	}
 	

commit 56d67097b0f2b6f86559e63b1cc1963d71bd219b

fixed incorrect variable name (introduced in changeset f2bdb7b6dd4738d7e9bc7717d7764a00d75c902e)

src/elrte/js/elRTE.filter.js
@@ -843,7 +843,7 @@
 			if (!this.xhtml) return html;
 
 			return html.replace(this.tagRegExp, function(fullMatch, closingSlash, tagName, tagAttributes) {
-				if (/^(img|hr|br|embed|param|link)$/i.test(name)) {
+				if (/^(img|hr|br|embed|param|link)$/i.test(tagName)) {
 					return '<' + tagName + tagAttributes + ' />';
 				} else {
 					return fullMatch;