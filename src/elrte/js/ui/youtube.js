0862ac55c55a4cb21e48944f5d146d2e3e9429a2
Foxel <foxel@quickfox.ru>
2014-01-16 12:31:04 +0700

    youtube video iframe insert update

--- src/elrte/js/ui/youtube.js
+++ src/elrte/js/ui/youtube.js
@@ -35,7 +35,7 @@
 		}
 
 		this.set = function(url, w, h) {
-			var youtubeID = (url.match(/youtu\.be\/(\w+)/) || url.match(/youtube.com\/watch\?(?:[^&]+&)*v=(\w+)/) || [])[1];
+			var youtubeID = (url.match(/youtu\.be\/([^&\?]+)/) || url.match(/youtube.com\/watch\?(?:[^&]+&)*v=([^&\?]+)/) || [])[1];
 			if (youtubeID) {
 				var toinsert = '<iframe width="'+w+'" height="'+h+'" src="http://www.youtube.com/embed/'+youtubeID+'?wmode=transparent" frameborder="0" allowfullscreen="true"> </iframe>';
 				var id = 'youtube-'+Math.random().toString().substring(2);
